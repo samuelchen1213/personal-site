c4eda8594cabdbcc1269d51750ef1be64b67fd02
samuelchen1213 <samuelchen23@icloud.com>
2021-04-24 13:27:23 -0400

    restructure files

---
+++ js/vanta-settings.js
@@ -0,0 +1,12 @@
+VANTA.TOPOLOGY({
+    el: "#vantaNet",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x564e9b,
+    backgroundColor: 0x222222
+  })
