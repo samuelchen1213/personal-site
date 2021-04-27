ee5e47f267b87efac2b6af585fdf38497dac7a11
samuelchen1213 <samuelchen23@icloud.com>
2021-04-27 19:28:16 -0400

    Cleanup animations and typing content

--- js/gsap-animations.js
+++ js/gsap-animations.js
@@ -13,71 +13,71 @@ TweenMax.to("#intro", 2, {
 
 
 TweenMax.from("#information, .zdog-container", 2.5, {
-    delay: baseDelay + 0.5,
+    delay: baseDelay + 1,
     y: 20,
     opacity: 0,
     ease: Power2.easeInOut
 });
 TweenMax.to("#information, .zdog-container", 2.5, {
-    delay: baseDelay + 0.5,
+    delay: baseDelay + 1,
     opacity: 1,
     ease: Power2.easeInOut
 });
 
 TweenMax.from("#learn", 2.5, {
-    delay: baseDelay + 1,
+    delay: baseDelay + 1.3,
     y: 20,
     opacity: 0,
     ease: Power2.easeInOut
 });
 TweenMax.to("#learn", 2.5, {
-    delay: baseDelay + 1,
+    delay: baseDelay + 1.3,
     opacity: 1,
     ease: Power2.easeInOut
 })
 
 
 TweenMax.from("#item1", 2.3, {
-    delay: baseDelay + 1.5,
+    delay: baseDelay + 1.6,
     y: 20,
     opacity: 0,
     ease: Power2.easeInOut
 });
 TweenMax.to("#item1", 2.3, {
-    delay: baseDelay + 1.5,
+    delay: baseDelay + 1.6,
     opacity: 1,
     ease: Power2.easeInOut
 })
 TweenMax.from("#item2", 2.3, {
-    delay: baseDelay + 1.7,
+    delay: baseDelay + 1.8,
     y: 20,
     opacity: 0,
     ease: Power2.easeInOut
 });
 TweenMax.to("#item2", 2.3, {
-    delay: baseDelay + 1.7,
+    delay: baseDelay + 1.8,
     opacity: 1,
     ease: Power2.easeInOut
 });
 TweenMax.from("#item3", 2, {
-    delay: baseDelay + 1.9,
+    delay: baseDelay + 2,
     y: 20,
     opacity: 0,
     ease: Power2.easeInOut
 });
 TweenMax.to("#item3", 2.3, {
-    delay: baseDelay + 1.9,
+    delay: baseDelay + 2,
     opacity: 1,
     ease: Power2.easeInOut
 });
 TweenMax.from("#item4", 2.3, {
-    delay: baseDelay + 2.1,
+    delay: baseDelay + 2.2,
     y: 20,
     opacity: 0,
     ease: Power2.easeInOut
 });
 TweenMax.to("#item4", 2.3, {
-    delay: baseDelay + 2.1,
+    delay: baseDelay + 2.2,
     opacity: 1,
     ease: Power2.easeInOut
 });
--- js/typing.js
+++ js/typing.js
@@ -1,48 +1,5 @@
 
-
-
-// // Greeting effect
-// const Typewriter = function(txtElement, words, wait = 3000) {
-//     this.txtElement = txtElement
-//     this.words = words;
-//     this.txt = '';
-//     this.wordIndex = 0;
-//     this.wait = parseInt(wait, 10);
-//     this.type();
-//     this.isDeleting = false;
-// }
-
-// Typewriter.prototype.type = function() {
-//     // Current index of word
-//     const current = this.wordIndex % this.words.length;
-//     // Get full text of current word
-//     const fullTxt = this.words[current];
-//     // Check if deleting
-//     if (this.isDeleting) {
-//         this.txt = fullTxt.substring(0, this.txt.length - 1)
-//     } else {
-//         this.txt = fullTxt.substring(0, this.txt.length + 1)
-//     }
-//     // Insert txt into element
-//     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-//     setTimeout(() => this.type(), 300)
-// }
-
-// document.addEventListener('DOMContentLoaded', bufferinit);
-
-// function init() {
-//     const txtElement = document.querySelector('.txt-type');
-//     const words = JSON.parse(txtElement.getAttribute('data-words'));
-//     const wait = txtElement.getAttribute('data-wait');
-//     // Init Typewriter
-//     new Typewriter(txtElement, words, wait);
-// }
-
-// function bufferinit() {
-//     setTimeout(init, 500);
-// }
-
-
+// Greeting Effect
 $(`document`).ready(function(){
     setTimeout(function(){
         var typed = new Typed('#greeting-typed', {
@@ -57,16 +14,18 @@ $(`document`).ready(function(){
 })
 
 
-
 // Intro Effect
 setTimeout(function greetingAnimation() {
     var typed = new Typed('#typed', {
         strings:[
-            'and I love to ideate',
-            'and I love to learn', 
-            'and I love to create'],
-        backSpeed: 70,
-        typeSpeed: 80,
+            'and I love to create.',
+            'and I love to lead.', 
+            'and I love to ideate.',
+            'and I love to learn.',
+            'and here\'s my site.'
+        ],
+        backSpeed: 40,
+        typeSpeed: 50,
         smartBackspace: true,
         backDelay: 1000
     })
