520826509a265d1e6661f3bdaf391a2e8875fc8b
samuelchen1213 <samuelchen23@icloud.com>
2021-05-04 13:42:49 -0400

    Shorten intro animation

--- js/intro-animations.js
+++ js/intro-animations.js
@@ -21,109 +21,135 @@ function greetingAnimation() {
                 'and I love to lead',
                 'and here\'s my site'
             ],
-            backSpeed: 40,
-            typeSpeed: 55,
+            backSpeed: 55,
+            typeSpeed: 75,
             smartBackspace: true,
-            backDelay: 1000
+            backDelay: 1500
         })
     }, 300)
 }
 
 tl = gsap.timeline({onComplete: greetingAnimation});
 tl.to("#overlay", 2.5, {
-        delay: 1.6,
+        delay: 1,
         opacity: 0,
         ease: Power2.easeInOut}, 1)
     .from('#intro', 2.5, {
-        delay: 1.5, 
-        y: 15, 
+        delay: 0.6, 
+        // y: 15, 
         opacity:0, 
         ease: Power4.easeOut}, 1)
     .to('#intro', 2.5, {
-        delay: 1.5, 
+        delay: 0.6, 
         ease: Power4.easeOut, 
         opacity: 1}, 1)
     .from(".copyright-footer, .logo, #infoicon", 2, {
-        delay: - 1.5,
+        delay: - 2,
         opacity: 0,
         ease: Power2.easeInOut})
     .to(".copyright-footer, .logo, #infoicon", 2, {
-        delay: - 1.5,
+        delay: - 2,
         opacity: 1,
         ease: Power2.easeInOut
     });
 
-let baseDelay = 2.5;
+let baseDelay = 1.7;
 
-TweenMax.from("#information, .zdog-container", 2.5, {
-    delay: baseDelay + 1,
+TweenMax.from("#p1", 2.5, {
+    delay: baseDelay,
+    // y: 15, 
     opacity: 0,
     ease: Power2.easeInOut
 });
-TweenMax.to("#information, .zdog-container", 2.5, {
-    delay: baseDelay + 1,
+TweenMax.to("#p1", 2.5, {
+    delay: baseDelay,
+    opacity: 1,
+    ease: Power2.easeInOut
+});
+
+TweenMax.from("#p2", 2.5, {
+    delay: baseDelay + 0.2,
+    // y: 15, 
+    opacity: 0,
+    ease: Power2.easeInOut
+});
+TweenMax.to("#p2", 2.5, {
+    delay: baseDelay + 0.2,
+    opacity: 1,
+    ease: Power2.easeInOut
+});
+
+TweenMax.from("#p3, .zdog-container", 2.5, {
+    delay: baseDelay + 0.4,
+    // y: 15, 
+    opacity: 0,
+    ease: Power2.easeInOut
+});
+TweenMax.to("#p3, .zdog-container", 2.5, {
+    delay: baseDelay + 0.4,
     opacity: 1,
     ease: Power2.easeInOut
 });
 
 TweenMax.from("#learn", 3, {
-    delay: baseDelay + 1.5,
+    delay: baseDelay + 0.6,
+    // y: 15, 
     opacity: 0,
     ease: Power2.easeInOut
 });
 TweenMax.to("#learn", 3, {
-    delay: baseDelay + 1.5,
+    delay: baseDelay + 0.6,
     opacity: 1,
     ease: Power2.easeInOut
 })
 
 
-TweenMax.from("#item1", 2, {
+TweenMax.from("#item1", 1.5, {
     delay: baseDelay + 1.5,
     opacity: 0,
     ease: Power2.easeInOut
 });
-TweenMax.to("#item1", 2, {
+TweenMax.to("#item1", 1.5, {
     delay: baseDelay + 1.5,
     opacity: 1,
     ease: Power2.easeInOut
 })
-TweenMax.from("#item2", 2, {
+TweenMax.from("#item2", 1.5, {
     delay: baseDelay + 1.7,
     opacity: 0,
     ease: Power2.easeInOut
 });
-TweenMax.to("#item2", 2, {
+TweenMax.to("#item2", 1.5, {
     delay: baseDelay + 1.7,
     opacity: 1,
     ease: Power2.easeInOut
 });
-TweenMax.from("#item3", 2, {
+TweenMax.from("#item3", 1.5, {
     delay: baseDelay + 1.9,
     opacity: 0,
     ease: Power2.easeInOut
 });
-TweenMax.to("#item3", 2, {
+TweenMax.to("#item3", 1.5, {
     delay: baseDelay + 1.9,
     opacity: 1,
     ease: Power2.easeInOut
 });
-TweenMax.from("#item4", 2, {
+TweenMax.from("#item4", 1.5, {
     delay: baseDelay + 2.1,
     opacity: 0,
     ease: Power2.easeInOut
 });
-TweenMax.to("#item4", 2, {
+TweenMax.to("#item4", 1.5, {
     delay: baseDelay + 2.1,
     opacity: 1,
     ease: Power2.easeInOut
 });
-TweenMax.from("#item5", 2, {
+TweenMax.from("#item5", 1.5, {
     delay: baseDelay + 2.3,
     opacity: 0,
     ease: Power2.easeInOut
 });
-TweenMax.to("#item5", 2, {
+TweenMax.to("#item5", 1.5, {
     delay: baseDelay + 2.3,
     opacity: 1,
     ease: Power2.easeInOut
