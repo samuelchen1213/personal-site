171a1aced112fb02b55390cd8c0142621d9bcba0
samuelchen1213 <samuelchen23@icloud.com>
2021-05-12 13:53:05 -0400

    Add feedback on hover

--- js/custom-cursor.js
+++ js/custom-cursor.js
@@ -5,47 +5,41 @@ const updateProperties = (elem, state) => {
     elem.style.setProperty('--height', `${state.height}px`)
     elem.style.setProperty('--radius', state.radius)
     elem.style.setProperty('--scale', state.scale)
-  }
+    elem.style.setProperty('--color', state.color)
+}
   
   document.querySelectorAll('.cursor').forEach(cursor => {
-    let onElement
+    let onElement;
   
     const createState = e => {
       const defaultState = {
         x: e.clientX,
         y: e.clientY,
-        width: 40,
-        height: 40,
-        radius: '50%'
+        radius: '50%',
       }
-  
-      const computedState = {}
-  
-    //   if (onElement != null) {
-    //     const { top, left, width, height } = onElement.getBoundingClientRect()
-    //     const radius = window.getComputedStyle(onElement).borderTopLeftRadius
-  
-    //     computedState.x = left + width / 2
-    //     computedState.y = top + height / 2
-    //     computedState.width = width
-    //     computedState.height = height
-    //     computedState.radius = radius
-    //   }
-  
-      return {
-        ...defaultState,
-        ...computedState
+
+      if (onElement) {
+        defaultState.width = 45;
+        defaultState.height = 45;
+        defaultState.color = '#cc2255';
+      } else {
+        defaultState.width = 35;
+        defaultState.height = 35;
+        defaultState.color = '#b55400';
       }
+      return defaultState;
     }
   
     document.addEventListener('mousemove', e => {
-      const state = createState(e)
-      updateProperties(cursor, state)
+      const state = createState(e);
+      updateProperties(cursor, state);
     })
+
+    // $(document).click(()=>(onElement = true));
   
-    // document.querySelectorAll('a, button').forEach(elem => {
-    //   elem.addEventListener('mouseenter', () => (onElement = elem))
-    //   elem.addEventListener('mouseleave', () => (onElement = undefined))
-    // })
+    document.querySelectorAll('a, button, i, .close').forEach(elem => {
+      elem.addEventListener('mouseenter', ()=>(onElement = true));
+      elem.addEventListener('mouseleave', ()=>(onElement = false));
+    })
   })
   
