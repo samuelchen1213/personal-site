d9a4d7693d47a35ca8aba79bfcc3f346e91fa6de
samuelchen1213 <samuelchen23@icloud.com>
2021-05-12 15:14:41 -0400

    Setup cursor 2

--- js/custom-cursor.js
+++ js/custom-cursor.js
@@ -1,45 +1,36 @@
-const updateProperties = (elem, state) => {
-    elem.style.setProperty('--x', `${state.x}px`)
-    elem.style.setProperty('--y', `${state.y}px`)
-    elem.style.setProperty('--width', `${state.width}px`)
-    elem.style.setProperty('--height', `${state.height}px`)
-    elem.style.setProperty('--radius', state.radius)
-    elem.style.setProperty('--scale', state.scale)
-    elem.style.setProperty('--color', state.color)
-}
-  
-  document.querySelectorAll('.cursor').forEach(cursor => {
-    let onElement;
-  
-    const createState = e => {
-      const defaultState = {
-        x: e.clientX,
-        y: e.clientY,
-        radius: '50%',
-      }
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+var a = document.querySelectorAll('a');
 
-      if (onElement) {
-        defaultState.width = 45;
-        defaultState.height = 45;
-        defaultState.color = '#cc2255';
-      } else {
-        defaultState.width = 35;
-        defaultState.height = 35;
-        defaultState.color = '#b55400';
-      }
-      return defaultState;
-    }
-  
-    document.addEventListener('mousemove', e => {
-      const state = createState(e);
-      updateProperties(cursor, state);
-    })
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+});
 
-    // $(document).click(()=>(onElement = true));
-  
-    document.querySelectorAll('a, button, i, .close').forEach(elem => {
-      elem.addEventListener('mouseenter', ()=>(onElement = true));
-      elem.addEventListener('mouseleave', ()=>(onElement = false));
-    })
-  })
-  
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+  });
+})
